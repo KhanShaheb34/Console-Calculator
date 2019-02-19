5eed46b9c6d400e077cea86d1d840d14092d28c9
Shakirul Hasan <theshk.me@gmail.com>
2019-02-20 02:09:59 +0600

    Console Calculator Beta Added

---
+++ src/Calculate/Calculator.java
@@ -0,0 +1,110 @@
+package Calculate;
+
+public class Calculator {
+    private double a;
+    private double b;
+    private char s;
+    private int position;
+
+    private Logic calculatorLogic = new Logic();
+
+    public Calculator() {
+        a = 0;
+        b = 0;
+        position = 0;
+    }
+
+    private double calculate(double a, double b, char s) {
+        switch (s) {
+            case '+':
+                return calculatorLogic.add(a, b);
+            case '-':
+                return calculatorLogic.sub(a, b);
+            case '*':
+                return calculatorLogic.mul(a, b);
+            case '/':
+                return calculatorLogic.div(a, b);
+            case '%':
+                return calculatorLogic.mod(a, b);
+            case '^':
+                return calculatorLogic.pow(a, b);
+            case 't':
+                return calculatorLogic.tan(a);
+            case 's':
+                return calculatorLogic.sin(a);
+            case 'e':
+                return calculatorLogic.sec(a);
+            case 'c':
+                return calculatorLogic.cosec(a);
+            case 'o':
+                return calculatorLogic.cos(a);
+            case 'a':
+                return calculatorLogic.cot(a);
+            case 'r':
+                return calculatorLogic.sqrt(a);
+            default:
+                System.out.println("Please input a valid sign!");
+                return 0;
+
+        }
+    }
+
+    private void stringToCommand(String command){
+        if(isInt(command.charAt(0))){
+            a = parseInt(command);
+            while(command.charAt(position)==' ') position++;
+            s = command.charAt(position);
+            while(!isInt(command.charAt(position))) position++;
+            b = parseInt(command);
+        }
+        else {
+            switch (command.charAt(0)){
+                case 't':
+                    while(!isInt(command.charAt(position))) position++;
+                    a = parseInt(command);
+                    s = 't';
+                    break;
+                case 's':
+                    while(!isInt(command.charAt(position))) position++;
+                    a = parseInt(command);
+                    if (command.charAt(1) == 'i') s = 's';
+                    else s = 'e';
+                    break;
+                case 'c':
+                    while(!isInt(command.charAt(position))) position++;
+                    a = parseInt(command);
+                    if (command.charAt(3) == 'e') s = 'c';
+                    else if(command.charAt(2) == 't') s = 'a';
+                    else s = 'o';
+                    break;
+                case 'r':
+                    while(!isInt(command.charAt(position))) position++;
+                    a = parseInt(command);
+                    s = 'r';
+                    break;
+            }
+            b = 0;
+        }
+    }
+
+    private double parseInt(String command) {
+        double temp = 0;
+        for (;;) {
+            temp *= 10;
+            temp += command.charAt(position)-'0';
+            position++;
+            if(!isInt(command.charAt(position))) break;
+        }
+        return temp;
+    }
+
+    public double action(String command) {
+        stringToCommand(command);
+        return calculate(a, b, s);
+    }
+
+    private boolean isInt(char ch) {
+        return ch >= 48 && ch < 58;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import Calculate.Calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            Calculator calculator = new Calculator();
+            System.out.print(">>> ");
+            String command = scanner.nextLine();
+            command += ".";
+            double ans = calculator.action(command);
+            if((ans*10)%10 == 0) System.out.println((long)ans);
+            else System.out.println(ans);
+        }
+    }
+}
